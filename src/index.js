44c7e0abfd85d1582493f7f2ddba078f051d0499
Alvina <Es_96@mail.ru>
2023-01-13 12:53:26 +0700

    feat: complete the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    let arr = String(Math.abs(n))
+    let arr1 = arr.split('').reverse();
+    return Number(arr1.join(''));
 }
